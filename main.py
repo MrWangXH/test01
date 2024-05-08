cd28b26f02e1afff867027bea7d80dabc1c49a38
MrWangXH <169028044+MrWangXH@users.noreply.github.com>
2024-05-08 17:30:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# 导入需要用到的package
+import numpy as np
+import json
+# 读入训练数据
+datafile = './work/housing.data'
+data = np.fromfile(datafile, sep=' ')
+np.array([6.320e-03, 1.800e+01, 2.310e+00, ..., 3.969e+02, 7.880e+00, 1.190e+01])
+
+# 读入之后的数据被转化成1维array，其中array的第0-13项是第一条数据，第14-27项是第二条数据，以此类推....
+# 这里对原始数据做reshape，变成N x 14的形式
+feature_names = [ 'CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE','DIS',
+                 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV' ]
+feature_num = len(feature_names)
+data = data.reshape([data.shape[0] // feature_num, feature_num])
